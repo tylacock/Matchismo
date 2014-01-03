46aac55f880acfadba5d59cdefd0745ea86d600d
tylacock <tylacock@gmail.com>
2014-01-03 15:00:47 -0500

    first commit

---
+++ Matchismo/Card.h
@@ -0,0 +1,18 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Lacock, Ty on 1/3/14.
+//  Copyright (c) 2014 TWL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@property (strong, nonatomic) NSString *contents;
+
+@property (nonatomic, getter = isChosen) BOOL chosen;
+@property (nonatomic, getter = isMatched) BOOL matched;
+
+@end
---
+++ Matchismo/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Lacock, Ty on 1/3/14.
+//  Copyright (c) 2014 TWL. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
