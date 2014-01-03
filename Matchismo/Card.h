286dc3addf10d107b9d9e99a723fbe8d8898f21b
tylacock <tylacock@gmail.com>
2014-01-03 15:12:27 -0500

    Finish Lecture 1

--- Matchismo/Card.h
+++ Matchismo/Card.h
@@ -15,4 +15,5 @@
 @property (nonatomic, getter = isChosen) BOOL chosen;
 @property (nonatomic, getter = isMatched) BOOL matched;
 
+- (int)match:(NSArray *)otherCards;
 @end
--- Matchismo/Card.m
+++ Matchismo/Card.m
@@ -10,4 +10,18 @@
 
 @implementation Card
 
+- (int)match:(NSArray *)otherCards
+{
+    int score = 0;
+    
+    for (Card *card in otherCards) {
+        if ([card.contents isEqualToString:self.contents]) {
+            score = 1;
+        }
+    }
+    
+    
+    return score;
+}
+
 @end
